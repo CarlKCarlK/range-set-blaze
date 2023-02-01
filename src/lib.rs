f639655bdf1275e8a384dceec5e19fe113a546a9
Carl Kadie <carlk@msn.com>
2023-01-31 17:04:53 -0800

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+// https://docs.rs/range_bounds_map/latest/range_bounds_map/range_bounds_set/struct.RangeBoundsSet.html
+// Here are some relevant crates I found whilst searching around the topic area:
+
+// https://docs.rs/rangemap Very similar to this crate but can only use Ranges and RangeInclusives as keys in it's map and set structs (separately).
+// https://docs.rs/btree-range-map
+// https://docs.rs/ranges Cool library for fully-generic ranges (unlike std::ops ranges), along with a Ranges datastructure for storing them (Vec-based unfortunately)
+// https://docs.rs/intervaltree Allows overlapping intervals but is immutable unfortunately
+// https://docs.rs/nonoverlapping_interval_tree Very similar to rangemap except without a gaps() function and only for Ranges and not RangeInclusives. And also no fancy coalescing functions.
+// https://docs.rs/unbounded-interval-tree A data structure based off of a 2007 published paper! It supports any RangeBounds as keys too, except it is implemented with a non-balancing Box<Node> based tree, however it also supports overlapping RangeBounds which my library does not.
+// https://docs.rs/rangetree I'm not entirely sure what this library is or isn't, but it looks like a custom red-black tree/BTree implementation used specifically for a Range Tree. Interesting but also quite old (5 years) and uses unsafe.
+// https://docs.rs/btree-range-map/latest/btree_range_map/
+// Related: https://lib.rs/crates/iset
+// https://lib.rs/crates/interval_tree
+// https://lib.rs/crates/range-set
+// https://lib.rs/crates/rangemap
+// https://lib.rs/crates/ranges
+// https://lib.rs/crates/nonoverlapping_interval_tree
+
 mod tests;
 
 use itertools::Itertools;
@@ -5,20 +23,38 @@ use std::cmp::max;
 use std::collections::BTreeMap;
 use std::convert::From;
 use std::fmt;
-use std::ops::BitOr;
+use std::ops::{BitOr, Sub};
+use std::str::FromStr;
 
-pub fn fmt(items: &BTreeMap<u128, u128>) -> String {
+trait Integer:
+    num_integer::Integer
+    + fmt::Display
+    + fmt::Debug
+    + std::iter::Sum
+    + num_traits::Num
+    + num_traits::NumRef
+    + num_traits::NumOps
+    + num_traits::NumAssignOps
+    + FromStr
+{
+}
+
+pub fn fmt<T: Integer>(items: &BTreeMap<T, T>) -> String {
     items
         .iter()
         .map(|(start, end)| format!("{start}..{end}"))
         .join(",")
 }
 
-fn len_slow(items: &BTreeMap<u128, u128>) -> u128 {
+/// !!! cmk understand this
+fn len_slow<T: Integer>(items: &BTreeMap<T, T>) -> T
+where
+    for<'a> &'a T: Sub<&'a T, Output = T>,
+{
     items.iter().map(|(start, end)| end - start).sum()
 }
 
-pub fn internal_add(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u128, end: u128) {
+pub fn internal_add<T: Integer>(items: &mut BTreeMap<T, T>, len: &mut T, start: T, end: T) {
     assert!(start < end); // !!!cmk check that length is not zero
                           // !!! cmk would be nice to have a partition_point function that returns two iterators
     let mut before = items.range_mut(..=start).rev();
@@ -40,7 +76,7 @@ pub fn internal_add(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u12
     }
 }
 
-fn delete_extra(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u128, end: u128) {
+fn delete_extra<T: Integer>(items: &mut BTreeMap<T, T>, len: &mut T, start: T, end: T) {
     let mut after = items.range_mut(start..);
     let (start_after, start_end) = after.next().unwrap(); // !!! cmk assert that there is a next
     assert!(start == *start_after && end == *start_end); // !!! cmk real assert
@@ -65,7 +101,7 @@ fn delete_extra(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u128, e
         items.remove(&start);
     }
 }
-fn insert(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u128, end: u128) {
+fn insert<T: Integer>(items: &mut BTreeMap<T, T>, len: &mut T, start: T, end: T) {
     let was_there = items.insert(start, end);
     assert!(was_there.is_none());
     // !!!cmk real assert
@@ -76,59 +112,62 @@ fn insert(items: &mut BTreeMap<u128, u128>, len: &mut u128, start: u128, end: u1
 // !!!cmk allow negatives and any size
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
-pub struct RangeSetInt {
-    len: u128,
-    items: BTreeMap<u128, u128>, // !!!cmk usize?
+pub struct RangeSetInt<T: Integer> {
+    len: T,
+    items: BTreeMap<T, T>, // !!!cmk usize?
 }
 
 // !!!cmk support =, and single numbers
 // !!!cmk error to use -
 // !!!cmk are the unwraps OK?
 // !!!cmk what about bad input?
-impl From<&str> for RangeSetInt {
+impl<T: Integer> From<&str> for RangeSetInt<T> {
     fn from(s: &str) -> Self {
         let mut result = RangeSetInt::new();
         for range in s.split(',') {
             let mut range = range.split("..");
-            let start = range.next().unwrap().parse::<u128>().unwrap();
-            let end = range.next().unwrap().parse::<u128>().unwrap();
+            let start = range.next().unwrap().parse::<T>().unwrap();
+            let end = range.next().unwrap().parse::<T>().unwrap();
             result.internal_add(start, end);
         }
         result
     }
 }
 
-impl fmt::Debug for RangeSetInt {
+impl<T: Integer> fmt::Debug for RangeSetInt<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", fmt(&self.items))
     }
 }
 
-impl fmt::Display for RangeSetInt {
+impl<T: Integer> fmt::Display for RangeSetInt<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", fmt(&self.items))
     }
 }
 
-impl RangeSetInt {
-    pub fn new() -> RangeSetInt {
+impl<T: Integer> RangeSetInt<T> {
+    pub fn new() -> RangeSetInt<T> {
         RangeSetInt {
             items: BTreeMap::new(),
-            len: 0,
+            len: T::zero(),
         }
     }
 
     pub fn clear(&mut self) {
         self.items.clear();
-        self.len = 0;
+        self.len = T::zero();
     }
 
     // !!!cmk keep this in a field
-    pub fn len(&self) -> u128 {
+    pub fn len(&self) -> T {
         self.len
     }
 
-    fn len_slow(&self) -> u128 {
+    fn len_slow(&self) -> T
+    where
+        for<'a> &'a T: Sub<&'a T, Output = T>,
+    {
         len_slow(&self.items)
     }
 
@@ -171,7 +210,7 @@ impl RangeSetInt {
     /// assert_eq!(set.contains(1), true);
     /// assert_eq!(set.contains(4), false);
     /// ```
-    pub fn contains(&self, value: u128) -> bool {
+    pub fn contains(&self, value: T) -> bool {
         self.items
             .range(..=value)
             .next_back()
@@ -180,72 +219,10 @@ impl RangeSetInt {
 
     // https://stackoverflow.com/questions/49599833/how-to-find-next-smaller-key-in-btreemap-btreeset
     // https://stackoverflow.com/questions/35663342/how-to-modify-partially-remove-a-range-from-a-btreemap
-    fn internal_add(&mut self, start: u128, end: u128) {
+    fn internal_add(&mut self, start: T, end: T) {
         internal_add(&mut self.items, &mut self.len, start, end);
     }
 
-    // fn _internal_add(&mut self, start: u128, length: u128) {
-    //     // !!!cmk put this shortcut back?
-    //     // if self._items.len() == 0 {
-    //     //     self._items.insert(start, length);
-    //     //     return;
-    //     // }
-
-    //     // !!!cmk rename index to "range"
-    //     let range = self._items.range(..start);
-    //     let mut peekable_forward = range.clone().peekable();
-    //     let peek_forward = peekable_forward.peek();
-    //     let mut peekable_backwards = range.rev().peekable();
-    //     let peek_backwards = peekable_backwards.peek();
-    //     if let Some(peek_forward) = peek_forward {
-    //         let mut peek_forward = *peek_forward;
-    //         if *peek_forward.0 == start {
-    //             if length > *peek_forward.1 {
-    //                 peek_forward.1 = &length;
-    //                 // previous_range = peek_forward;
-    //                 // peek_forward = peekable_forward.next(); // index should point to the following range for the remainder of this method
-    //                 todo!()
-    //             } else {
-    //                 todo!();
-    //             }
-    //         }
-    //     } else {
-    //         println!("self._items.insert(start, length);");
-    //         if let Some(previous_range) = peek_backwards {
-    //             // nothing
-    //         } else {
-    //             return;
-    //         }
-    //     }
-
-    //     todo!();
-    //     //             return;
-    //     //         }
-    //     //     } else if index == 0 {
-    //     //         self._items.insert(index, RangeX { start, length });
-    //     //         previous_index = index;
-    //     //         index += 1 // index_of_miss should point to the following range for the remainder of this method
-    //     //     } else {
-    //     //         previous_index = index - 1;
-    //     //         let previous_range: &mut RangeX = &mut self._items[previous_index];
-
-    //     //         if previous_range.end() >= start {
-    //     //             let new_length = start + length - previous_range.start;
-    //     //             if new_length <= previous_range.length {
-    //     //                 return;
-    //     //             } else {
-    //     //                 previous_range.length = new_length;
-    //     //             }
-    //     //         } else {
-    //     //             // after previous range, not contiguous with previous range
-    //     //             self._items.insert(index, RangeX { start, length });
-    //     //             previous_index = index;
-    //     //             index += 1;
-    //     //         }
-    //     //     }
-    //     // }
-
-    //     // let previous_range: &RangeX = &self._items[previous_index];
     //     // let previous_end = previous_range.end();
     //     // while index < self._items.len() {
     //     //     let range: &RangeX = &self._items[index];
@@ -264,8 +241,8 @@ impl RangeSetInt {
     // }
 }
 
-impl BitOr<&RangeSetInt> for &RangeSetInt {
-    type Output = RangeSetInt;
+impl<T: Integer> BitOr<&RangeSetInt<T>> for &RangeSetInt<T> {
+    type Output = RangeSetInt<T>;
 
     /// Returns the union of `self` and `rhs` as a new `RangeSetInt`.
     ///
@@ -280,7 +257,7 @@ impl BitOr<&RangeSetInt> for &RangeSetInt {
     /// let result = &a | &b;
     /// assert_eq!(result, RangeSetInt::from([1, 2, 3, 4, 5]));
     /// ```
-    fn bitor(self, rhs: &RangeSetInt) -> RangeSetInt {
+    fn bitor(self, rhs: &RangeSetInt<T>) -> RangeSetInt<T> {
         let mut result = self.clone();
         for (start, end) in rhs.items.iter() {
             result.internal_add(*start, *end);
@@ -289,19 +266,44 @@ impl BitOr<&RangeSetInt> for &RangeSetInt {
     }
 }
 
-impl<const N: usize> From<[u128; N]> for RangeSetInt {
-    fn from(arr: [u128; N]) -> Self {
+// !!! cmk
+// impl<T: Ord + Clone, A: Allocator + Clone> BitAnd<&BTreeSet<T, A>> for &BTreeSet<T, A> {
+//     type Output = BTreeSet<T, A>;
+
+//     /// Returns the intersection of `self` and `rhs` as a new `BTreeSet<T>`.
+//     ///
+//     /// # Examples
+//     ///
+//     /// ```
+//     /// use std::collections::BTreeSet;
+//     ///
+//     /// let a = BTreeSet::from([1, 2, 3]);
+//     /// let b = BTreeSet::from([2, 3, 4]);
+//     ///
+//     /// let result = &a & &b;
+//     /// assert_eq!(result, BTreeSet::from([2, 3]));
+//     /// ```
+//     fn bitand(self, rhs: &BTreeSet<T, A>) -> BTreeSet<T, A> {
+//         BTreeSet::from_sorted_iter(
+//             self.intersection(rhs).cloned(),
+//             ManuallyDrop::into_inner(self.map.alloc.clone()),
+//         )
+//     }
+// }
+
+impl<T: Integer, const N: usize> From<[T; N]> for RangeSetInt<T> {
+    fn from(arr: [T; N]) -> Self {
         let mut result = RangeSetInt::new();
         for value in arr.iter() {
-            result.internal_add(*value, *value + 1);
+            result.internal_add(*value, *value + T::one());
         }
         result
     }
 }
 
-impl IntoIterator for RangeSetInt {
-    type Item = u128;
-    type IntoIter = IntoIter;
+impl<T: Integer> IntoIterator for RangeSetInt<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
 
     /// Gets an iterator for moving out the `RangeSetInt`'s contents.
     ///
@@ -315,21 +317,21 @@ impl IntoIterator for RangeSetInt {
     /// let v: Vec<_> = set.into_iter().collect();
     /// assert_eq!(v, [1, 2, 3, 4]);
     /// ```
-    fn into_iter(self) -> IntoIter {
+    fn into_iter(self) -> IntoIter<T> {
         IntoIter {
-            item_iter: 0..0,
+            item_iter: T::zero()..T::zero(),
             range_iter: self.items.into_iter(),
         }
     }
 }
 
-pub struct IntoIter {
-    item_iter: core::ops::Range<u128>,
-    range_iter: std::collections::btree_map::IntoIter<u128, u128>,
+pub struct IntoIter<T: Integer> {
+    item_iter: core::ops::Range<T>,
+    range_iter: std::collections::btree_map::IntoIter<T, T>,
 }
 
-impl Iterator for IntoIter {
-    type Item = u128;
+impl<T: Integer> Iterator for IntoIter<T> {
+    type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(item) = self.item_iter.next() {
