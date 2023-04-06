8eeb170529013c13526ef4b4240ab3f3d26cfb6e
Carl Kadie <carlk@msn.com>
2023-04-06 10:15:23 -0700

    use csv in example

--- examples/missing2.rs
+++ examples/missing2.rs
@@ -1,18 +1,17 @@
+use csv::ReaderBuilder;
 use glob::glob;
 use range_set_blaze::prelude::*;
-use std::{
-    error::Error,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut all_exps = RangeSetBlaze::from_iter([0..=99_999_999]);
 
     for path in glob("examples/cluster_file.*.tsv")? {
-        let exp_nums: RangeSetBlaze<_> = BufReader::new(File::open(path?)?)
-            .lines()
-            .filter_map(|line| line.ok()?.split_once('\t')?.0.parse::<u32>().ok())
+        let exp_nums: RangeSetBlaze<_> = ReaderBuilder::new()
+            .delimiter(b'\t')
+            .from_path(path?)?
+            .into_records()
+            .filter_map(|line| line.ok()?.get(0)?.parse::<u32>().ok())
             .collect();
         all_exps = all_exps - exp_nums;
     }
