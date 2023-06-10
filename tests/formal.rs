4b3fdc9aabdef51b2c6bd7fd7953904d9f366cf2
Carl Kadie <carlk@msn.com>
2023-06-10 14:37:29 -0700

    update formal.rs

--- tests/formal.rs
+++ tests/formal.rs
@@ -35,29 +35,29 @@
 //     estimate_size(x);
 // }
 
-fn estimate_size(x: u32) -> u32 {
-    assert!(x < 4096);
+// fn estimate_size(x: u32) -> u32 {
+//     assert!(x < 4096);
 
-    if x < 256 {
-        if x < 128 {
-            return 1;
-        } else {
-            return 3;
-        }
-    } else if x < 1024 {
-        if x > 1022 {
-            return 4;
-        } else {
-            return 5;
-        }
-    } else {
-        if x < 2048 {
-            return 7;
-        } else {
-            return 9;
-        }
-    }
-}
+//     if x < 256 {
+//         if x < 128 {
+//             return 1;
+//         } else {
+//             return 3;
+//         }
+//     } else if x < 1024 {
+//         if x > 1022 {
+//             return 4;
+//         } else {
+//             return 5;
+//         }
+//     } else {
+//         if x < 2048 {
+//             return 7;
+//         } else {
+//             return 9;
+//         }
+//     }
+// }
 
 // #[cfg(kani)]
 // #[kani::proof]
