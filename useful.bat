b7345813ee8a4ae555b9346bcbff6d30b1c7d3be
Carl Kadie <carlk@msn.com>
2023-06-13 06:56:05 -0700

    checkpoint

---
+++ useful.bat
@@ -0,0 +1,22 @@
+# tests
+cargo test --doc intersection_dyn
+cargo test coverage -- --nocapture
+
+# examples
+cargo run --example parity
+cargo run --example missing
+
+# Docs
+cargo doc --no-deps --open & cargo test --doc
+
+# coverage
+cargo llvm-cov --open
+target\llvm-cov\html\index.html
+
+# bench
+cargo bench worst & target\criterion\report\index.html
+cargo bench overflow & target\criterion\overflow\report\index.html 
+python benches\summary.py > benches\summary.tsv
+
+# publish
+cargo publish --dry-run
