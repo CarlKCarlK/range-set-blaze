6292f90b3129ca61ee7a17007fcb5c21207d1d83
Carl Kadie <carlk@msn.com>
2023-11-28 17:05:23 -0800

    some generics are working in is_consecutive1

--- examples/simd/is_consecutive1/src/lib.rs
+++ examples/simd/is_consecutive1/src/lib.rs
@@ -1,37 +1,51 @@
 #![feature(portable_simd)]
 
 use core::cmp::PartialEq;
-use core::ops::Sub;
+use core::ops::{Add, Sub};
 use core::simd::{prelude::*, LaneCount, SimdElement, SupportedLaneCount};
 
-pub const LANES: usize = 16;
-
-pub fn is_consecutive_regular(chunk: &[u32; LANES]) -> bool {
-    for i in 1..LANES {
-        if chunk[i - 1] == u32::MAX || chunk[i - 1] + 1 != chunk[i] {
+#[inline]
+pub fn is_consecutive_regular<T, const N: usize>(chunk: &[T; N], one: T, max: T) -> bool
+where
+    T: SimdElement + Add + PartialEq,
+    T: std::ops::Add<Output = T>,
+{
+    for i in 1..N {
+        if chunk[i - 1] == max || chunk[i - 1] + one != chunk[i] {
             return false;
         }
     }
     true
 }
 
-const REFERENCE_SPLAT0: Simd<u32, LANES> =
-    Simd::from_array([15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
+// const REFERENCE_SPLAT0: Simd<T, N> =
+//     Simd::from_array([15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]);
 
-pub fn is_consecutive_splat0(chunk: Simd<u32, LANES>) -> bool {
-    if chunk[0].overflowing_add(LANES as u32 - 1) != (chunk[LANES - 1], false) {
-        return false;
-    }
-    let added = chunk + REFERENCE_SPLAT0;
-    Simd::<u32, LANES>::splat(added[0]) == added
-}
+// cmk00
+// pub fn is_consecutive_splat0<T, const N: usize>(chunk: Simd<T, N>, reference: Simd<T, N>) -> bool
+// where
+//     T: SimdElement,
+//     LaneCount<N>: SupportedLaneCount,
+//     Simd<T, N>: Sub<Output = Simd<T, N>>,
+//     Simd<T, N>: PartialEq<Simd<T, N>>,
+// {
+//     if chunk[0].overflowing_add(N as T - 1) != (chunk[N - 1], false) {
+//         return false;
+//     }
+//     let added = chunk + reference;
+//     Simd::<T, N>::splat(added[0]) == added
+// }
 
-const REFERENCE_SPLAT1: Simd<u32, LANES> =
-    Simd::from_array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
-
-pub fn is_consecutive_splat1(chunk: Simd<u32, LANES>) -> bool {
-    let subtracted = chunk - REFERENCE_SPLAT1;
-    Simd::<u32, LANES>::splat(chunk[0]) == subtracted
+#[inline]
+pub fn is_consecutive_splat1<T, const N: usize>(chunk: Simd<T, N>, reference: Simd<T, N>) -> bool
+where
+    T: SimdElement,
+    LaneCount<N>: SupportedLaneCount,
+    Simd<T, N>: Sub<Output = Simd<T, N>>,
+    Simd<T, N>: PartialEq<Simd<T, N>>,
+{
+    let subtracted = chunk - reference;
+    Simd::<T, N>::splat(chunk[0]) == subtracted
 }
 
 macro_rules! reference_splat {
@@ -65,43 +79,60 @@ where
     Simd::<T, N>::splat(subtracted[0]) == subtracted
 }
 
-pub fn is_consecutive_sizzle(chunk: Simd<u32, LANES>) -> bool {
-    let subtracted = chunk - REFERENCE_SPLAT1;
-    simd_swizzle!(subtracted, [0; LANES]) == subtracted
-}
+// cmk00
+// pub fn is_consecutive_sizzle<T, const N: usize>(chunk: Simd<T, N>, reference: Simd<T, N>) -> bool
+// where
+//     T: SimdElement,
+//     LaneCount<N>: SupportedLaneCount,
+//     Simd<T, N>: Sub<Output = Simd<T, N>>,
+//     Simd<T, N>: PartialEq<Simd<T, N>>,
+// {
+//     let subtracted = chunk - reference;
+//     simd_swizzle!(subtracted, [0; N]) == subtracted
+// }
 
-const REFERENCE_ROTATE: Simd<u32, LANES> =
-    Simd::from_array([4294967281, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+// const REFERENCE_ROTATE: Simd<T, N> =
+//     Simd::from_array([4294967281, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
-pub fn is_consecutive_rotate(chunk: Simd<u32, LANES>) -> bool {
+pub fn is_consecutive_rotate<T, const N: usize>(chunk: Simd<T, N>, reference: Simd<T, N>) -> bool
+where
+    T: SimdElement,
+    LaneCount<N>: SupportedLaneCount,
+    Simd<T, N>: Sub<Output = Simd<T, N>>,
+    Simd<T, N>: PartialEq<Simd<T, N>>,
+{
     let rotated = chunk.rotate_lanes_right::<1>();
-    chunk - rotated == REFERENCE_ROTATE
+    chunk - rotated == reference
 }
 
 #[test]
 fn test_is_consecutive() {
-    use std::array;
+    use syntactic_for::syntactic_for;
 
-    let a: [u32; LANES] = array::from_fn(|i| 100 + i as u32);
-    let ninety_nines: [u32; LANES] = [99; LANES];
-    assert!(is_consecutive_regular(&a));
-    assert!(!is_consecutive_regular(&ninety_nines));
+    syntactic_for! {LANES in [2, 4, 8, 16, 32, 64]  {$(
 
-    let a = Simd::from_array(a);
-    let ninety_nines = Simd::from_array(ninety_nines);
+        let a: Vec<u32> = (100u32..100 + $LANES).collect();
+        let ninety_nines: Vec<u32> = vec![99; $LANES];
+        let a = Simd::<u32, $LANES>::from_slice(&a);
+        let ninety_nines = Simd::<u32, $LANES>::from_slice(ninety_nines.as_slice());
 
-    assert!(is_consecutive_splat0(a));
-    assert!(!is_consecutive_splat0(ninety_nines));
+        assert!(is_consecutive_regular(a.as_array(), 1, u32::MAX));
+        assert!(!is_consecutive_regular(ninety_nines.as_array(), 1, u32::MAX));
 
-    assert!(is_consecutive_splat1(a));
-    assert!(!is_consecutive_splat1(ninety_nines));
 
-    assert!(is_consecutive_splat2(a, reference_splat_u32()));
-    assert!(!is_consecutive_splat2(ninety_nines, reference_splat_u32()));
+        // assert!(is_consecutive_splat0(a));
+        // assert!(!is_consecutive_splat0(ninety_nines));
 
-    assert!(is_consecutive_sizzle(a));
-    assert!(!is_consecutive_sizzle(ninety_nines));
+        assert!(is_consecutive_splat1(a));
+        assert!(!is_consecutive_splat1(ninety_nines));
 
-    assert!(is_consecutive_rotate(a));
-    assert!(!is_consecutive_rotate(ninety_nines));
+        assert!(is_consecutive_splat2(a, reference_splat_u32()));
+        assert!(!is_consecutive_splat2(ninety_nines, reference_splat_u32()));
+
+        // assert!(is_consecutive_sizzle(a));
+        // assert!(!is_consecutive_sizzle(ninety_nines));
+
+        // assert!(is_consecutive_rotate(a));
+        // assert!(!is_consecutive_rotate(ninety_nines));
+    )*}}
 }
