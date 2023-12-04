8d4f803cd2fb70458c2cbeb95c87333d115ce7e0
Carl Kadie <carlk@msn.com>
2023-12-04 12:30:33 -0800

    rename examples/simd folders

--- examples/simd/is_consecutive1/src/lib.rs
+++ examples/simd/is_consecutive1/src/lib.rs
@@ -1,66 +1,46 @@
 #![feature(portable_simd)]
 
 use std::{
-    ops::Add,
     ops::Sub,
     simd::{prelude::*, LaneCount, SimdElement, SupportedLaneCount},
 };
 
-#[macro_export]
-macro_rules! define_is_consecutive_regular {
-    ($function:ident, $type:ty) => {
-        #[inline]
-        pub fn $function<const N: usize>(chunk: &[$type; N]) -> bool
-        where
-            LaneCount<N>: SupportedLaneCount,
-        {
-            for i in 1..N {
-                if chunk[i - 1].checked_add(1) != Some(chunk[i]) {
-                    return false;
-                }
-            }
-            true
-        }
-    };
+// We can make our main function generic both for type and # of lanes:
+#[inline]
+pub fn is_consecutive_splat1_gen<T, const N: usize>(
+    chunk: Simd<T, N>,
+    reference: Simd<T, N>,
+) -> bool
+where
+    T: SimdElement + PartialEq,
+    Simd<T, N>: Sub<Simd<T, N>, Output = Simd<T, N>>,
+    LaneCount<N>: SupportedLaneCount,
+{
+    let subtracted = chunk - reference;
+    Simd::splat(chunk[0]) == subtracted
 }
 
-#[macro_export]
-macro_rules! define_is_consecutive_splat0 {
-    ($function:ident, $type:ty) => {
-        #[inline]
-        pub fn $function<const N: usize>(chunk: Simd<$type, N>) -> bool
-        where
-            $type: SimdElement,
-            Simd<$type, N>: Add<Simd<$type, N>, Output = Simd<$type, N>>,
-            LaneCount<N>: SupportedLaneCount,
-        {
-            define_reference_splat0!(reference_splat0, $type);
+// // But can we safely make the const reference_splat generic for type and # of lanes?
+// // Not currently, because we need From or One to be const.
+// use std::ops::AddAssign;
 
-            if chunk[0].overflowing_add(N as $type - 1) != (chunk[N - 1], false) {
-                return false;
-            }
-            let added = chunk + reference_splat0();
-            Simd::splat(added[0]) == added
-        }
-    };
-}
-#[macro_export]
-macro_rules! define_reference_splat0 {
-    ($function:ident, $type:ty) => {
-        pub const fn $function<const N: usize>() -> Simd<$type, N>
-        where
-            LaneCount<N>: SupportedLaneCount,
-        {
-            let mut arr: [$type; N] = [0; N];
-            let mut i = 0;
-            while i < N {
-                arr[i] = (N - i - 1) as $type;
-                i += 1;
-            }
-            Simd::from_array(arr)
-        }
-    };
-}
+// pub const fn reference_splat_gen<T, const N: usize>() -> Simd<T, N>
+// where
+//     T: SimdElement + Default + From<usize> + AddAssign,
+//     LaneCount<N>: SupportedLaneCount,
+// {
+//     let mut arr: [T; N] = [T::from(0usize); N];
+//     let mut i_usize = 0;
+//     while i_usize < N {
+//         arr[i_usize] = T::from(i_usize);
+//         i_usize += 1;
+//     }
+//     Simd::from_array(arr)
+// }
+
+// We can use a macro for the type and leave lane count a const generic.
+// However, in my case, I need to run on different types, but I need only one lane count at a time.
+// So, we use a macro from the type and a const for lane count.
 
 #[macro_export]
 macro_rules! define_is_consecutive_splat1 {
@@ -77,7 +57,6 @@ macro_rules! define_is_consecutive_splat1 {
         }
     };
 }
-
 #[macro_export]
 macro_rules! define_reference_splat {
     ($function:ident, $type:ty) => {
@@ -96,151 +75,45 @@ macro_rules! define_reference_splat {
     };
 }
 
-#[macro_export]
-macro_rules! define_is_consecutive_splat2 {
-    ($function:ident, $type:ty) => {
-        #[inline]
-        pub fn $function<const N: usize>(chunk: Simd<$type, N>) -> bool
-        where
-            LaneCount<N>: SupportedLaneCount,
-        {
-            define_reference_splat!(reference_splat, $type);
-
-            let subtracted = chunk - reference_splat();
-            Simd::splat(subtracted[0]) == subtracted
-        }
-    };
-}
+#[cfg(test)]
+use std::hint::black_box;
 
 #[test]
-fn test_regular() {
-    const LANES: usize = 16;
-    let a: Vec<i64> = (100..100 + LANES as i64).collect();
-    let ninety_nines: Vec<i64> = vec![99; LANES];
-    let a = Simd::<i64, LANES>::from_slice(&a);
-    let ninety_nines = Simd::<i64, LANES>::from_slice(ninety_nines.as_slice());
+fn test_is_consecutive_macros() {
+    use std::array;
 
-    assert!(IsConsecutive::is_consecutive_regular(a.as_array()));
-    assert!(!IsConsecutive::is_consecutive_regular(
-        ninety_nines.as_array()
-    ));
+    // Works on i32 and 16 lanes
+    define_is_consecutive_splat1!(is_consecutive_splat1_i32, i32);
+
+    let a: Simd<i32, 16> = black_box(Simd::from_array(array::from_fn(|i| 100 + i as i32)));
+    let ninety_nines: Simd<i32, 16> = black_box(Simd::from_array([99; 16]));
+    assert!(is_consecutive_splat1_i32(a));
+    assert!(!is_consecutive_splat1_i32(ninety_nines));
+
+    // Works on i8 and 64 lanes
+    define_is_consecutive_splat1!(is_consecutive_splat1_i8, i8);
+
+    let a: Simd<i8, 64> = black_box(Simd::from_array(array::from_fn(|i| 10 + i as i8)));
+    let ninety_nines: Simd<i8, 64> = black_box(Simd::from_array([99; 64]));
+    assert!(is_consecutive_splat1_i8(a));
+    assert!(!is_consecutive_splat1_i8(ninety_nines));
 }
 
-#[macro_export]
-macro_rules! define_is_consecutive_rotate {
-    ($function:ident, $type:ty) => {
-        #[inline]
-        pub fn $function<const N: usize>(chunk: Simd<$type, N>) -> bool
-        where
-            $type: SimdElement,
-            LaneCount<N>: SupportedLaneCount,
-        {
-            define_reference_rotate!(reference, $type);
+// cmk see https://godbolt.org/z/69dY1fvGj and see that it compiles well.
 
-            let rotated = chunk.rotate_lanes_right::<1>();
-            chunk - rotated == reference()
-        }
-    };
-}
-
-#[macro_export]
-macro_rules! define_reference_rotate {
-    ($function:ident, $type:ty) => {
-        #[inline]
-        pub const fn $function<const N: usize>() -> Simd<$type, N>
-        where
-            $type: SimdElement,
-            LaneCount<N>: SupportedLaneCount,
-        {
-            let mut arr: [$type; N] = [1; N];
-            arr[0] = (1 as $type).wrapping_sub(N as $type);
-            Simd::from_array(arr)
-        }
-    };
-}
-
-#[test]
-fn test_is_consecutive() {
-    use core::simd::Simd;
-
-    pub type Integer = i16;
-    const LANES: usize = 4;
-
-    let a: Vec<Integer> = (100..100 + LANES as Integer).collect();
-    let ninety_nines: Vec<Integer> = vec![99; LANES];
-    let a = Simd::<Integer, LANES>::from_slice(&a);
-    let ninety_nines = Simd::<Integer, LANES>::from_slice(ninety_nines.as_slice());
-
-    assert!(IsConsecutive::is_consecutive_regular(a.as_array()));
-    assert!(!IsConsecutive::is_consecutive_regular(
-        ninety_nines.as_array()
-    ));
-
-    assert!(IsConsecutive::is_consecutive_splat0(a));
-    assert!(!IsConsecutive::is_consecutive_splat0(ninety_nines));
-
-    assert!(IsConsecutive::is_consecutive_splat1(a));
-    assert!(!IsConsecutive::is_consecutive_splat1(ninety_nines));
-
-    assert!(IsConsecutive::is_consecutive_splat2(a));
-    assert!(!IsConsecutive::is_consecutive_splat2(ninety_nines));
-
-    assert!(IsConsecutive::is_consecutive_rotate(a));
-    assert!(!IsConsecutive::is_consecutive_rotate(ninety_nines));
-}
-
-pub trait IsConsecutive {
-    fn is_consecutive_regular<const N: usize>(chunk: &[Self; N]) -> bool
-    where
-        Self: Sized,
-        LaneCount<N>: SupportedLaneCount;
-
-    fn is_consecutive_splat0<const N: usize>(chunk: Simd<Self, N>) -> bool
+trait IsConsecutive {
+    fn is_consecutive<const N: usize>(chunk: Simd<Self, N>) -> bool
     where
         Self: SimdElement,
-        LaneCount<N>: SupportedLaneCount;
-
-    fn is_consecutive_splat1<const N: usize>(chunk: Simd<Self, N>) -> bool
-    where
-        Self: SimdElement,
-        LaneCount<N>: SupportedLaneCount;
-
-    fn is_consecutive_splat2<const N: usize>(chunk: Simd<Self, N>) -> bool
-    where
-        Self: SimdElement,
-        LaneCount<N>: SupportedLaneCount;
-
-    fn is_consecutive_rotate<const N: usize>(chunk: Simd<Self, N>) -> bool
-    where
-        Self: SimdElement,
+        Simd<Self, N>: Sub<Simd<Self, N>, Output = Simd<Self, N>>,
         LaneCount<N>: SupportedLaneCount;
 }
 
 macro_rules! impl_is_consecutive {
     ($type:ty) => {
         impl IsConsecutive for $type {
-            #[inline]
-            fn is_consecutive_regular<const N: usize>(chunk: &[$type; N]) -> bool
-            where
-                LaneCount<N>: SupportedLaneCount,
-            {
-                define_is_consecutive_regular!(is_consecutive_regular, $type);
-                is_consecutive_regular(chunk)
-            }
-
-            #[inline]
-            fn is_consecutive_splat0<const N: usize>(chunk: Simd<Self, N>) -> bool
-            where
-                Self: SimdElement,
-                Simd<Self, N>: Sub<Simd<Self, N>, Output = Simd<Self, N>>,
-                LaneCount<N>: SupportedLaneCount,
-            {
-                define_is_consecutive_splat0!(is_consecutive_splat0, $type);
-                is_consecutive_splat0(chunk)
-            }
-
-            #[inline]
-            fn is_consecutive_splat1<const N: usize>(chunk: Simd<Self, N>) -> bool
+            #[inline] // very important
+            fn is_consecutive<const N: usize>(chunk: Simd<Self, N>) -> bool
             where
                 Self: SimdElement,
                 Simd<Self, N>: Sub<Simd<Self, N>, Output = Simd<Self, N>>,
@@ -248,28 +121,6 @@ macro_rules! impl_is_consecutive {
             {
                 define_is_consecutive_splat1!(is_consecutive_splat1, $type);
                 is_consecutive_splat1(chunk)
-            }
-
-            #[inline]
-            fn is_consecutive_splat2<const N: usize>(chunk: Simd<Self, N>) -> bool
-            where
-                Self: SimdElement,
-                Simd<Self, N>: Add<Simd<Self, N>, Output = Simd<Self, N>>,
-                LaneCount<N>: SupportedLaneCount,
-            {
-                define_is_consecutive_splat2!(is_consecutive_splat2, $type);
-                is_consecutive_splat2(chunk)
-            }
-
-            #[inline]
-            fn is_consecutive_rotate<const N: usize>(chunk: Simd<Self, N>) -> bool
-            where
-                Self: SimdElement,
-                Simd<Self, N>: Add<Simd<Self, N>, Output = Simd<Self, N>>,
-                LaneCount<N>: SupportedLaneCount,
-            {
-                define_is_consecutive_rotate!(is_consecutive_rotate, $type);
-                is_consecutive_rotate(chunk)
             }
         }
     };
@@ -285,3 +136,22 @@ impl_is_consecutive!(u16);
 impl_is_consecutive!(u32);
 impl_is_consecutive!(u64);
 impl_is_consecutive!(usize);
+
+#[test]
+fn test_is_consecutive_trait() {
+    use std::array;
+
+    // Works on i32 and 16 lanes
+    let a: Simd<i32, 16> = black_box(Simd::from_array(array::from_fn(|i| 100 + i as i32)));
+    let ninety_nines: Simd<i32, 16> = black_box(Simd::from_array([99; 16]));
+
+    assert!(IsConsecutive::is_consecutive(a));
+    assert!(!IsConsecutive::is_consecutive(ninety_nines));
+
+    // Works on i8 and 64 lanes
+    let a: Simd<i8, 64> = black_box(Simd::from_array(array::from_fn(|i| 10 + i as i8)));
+    let ninety_nines: Simd<i8, 64> = black_box(Simd::from_array([99; 64]));
+
+    assert!(IsConsecutive::is_consecutive(a));
+    assert!(!IsConsecutive::is_consecutive(ninety_nines));
+}
