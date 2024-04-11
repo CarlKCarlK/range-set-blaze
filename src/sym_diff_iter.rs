d176236812c1be9f77d04be7a139aad618e16ac3
Carl Kadie <carlk@msn.com>
2024-04-11 09:10:03 -0700

    checkpoint

--- src/sym_diff_iter.rs
+++ src/sym_diff_iter.rs
@@ -1,9 +1,12 @@
+use alloc::collections::{btree_map::Range, BinaryHeap};
+
 use crate::{
-    merge::KMerge, Integer, Merge, SortedDisjoint, SortedStarts, SymDiffIterKMerge,
-    SymDiffIterMerge,
+    merge::KMerge, AssumeSortedStarts, Integer, Merge, SortedDisjoint, SortedStarts,
+    SymDiffIterKMerge, SymDiffIterMerge,
 };
 use core::{
-    cmp::{self, min},
+    array,
+    cmp::{max, Reverse},
     iter::FusedIterator,
     ops::RangeInclusive,
 };
@@ -41,21 +44,9 @@ where
     I: SortedStarts<T>,
 {
     iter: I,
-    next_item: Option<RangeInclusive<T>>,
-    workspace: Vec<RangeInclusive<T>>,
-    workspace_next_end: Option<T>,
-    gather: Option<RangeInclusive<T>>,
-    ready_to_go: Option<RangeInclusive<T>>,
-}
-
-fn min_next_end<T>(next_end: &Option<T>, next_item_end: T) -> Option<T>
-where
-    T: Integer,
-{
-    Some(next_end.map_or_else(
-        || next_item_end,
-        |current_end| cmp::min(current_end, next_item_end),
-    ))
+    start_or_min_value: T,
+    end_heap: BinaryHeap<Reverse<T>>,
+    dump_to: Option<T>,
 }
 
 impl<T, I> FusedIterator for SymDiffIter<T, I>
@@ -72,119 +63,182 @@ where
     I: SortedStarts<T>,
 {
     type Item = RangeInclusive<T>;
+    // cmk0 does this and UnionIter do the right thing on non-fused input?
 
     fn next(&mut self) -> Option<RangeInclusive<T>> {
-        // Keep doing this until we have something to return.
         loop {
-            if let Some(value) = self.ready_to_go.take() {
-                // If ready_to_go is Some, return the value immediately.
-                return Some(value);
+            let Some(next_range) = self.iter.next() else {
+                if self.end_heap.is_empty() {
+                    return None;
+                }
+                assert!(self.end_heap.len() == 1); // cmk
+                let end = self.end_heap.pop().unwrap().0;
+                assert!(self.end_heap.is_empty()); // cmk
+                return Some(self.start_or_min_value..=end);
             };
 
-            // if self.next_item should go into the workspace, then put it there, get the next, next_item, and loop
-            if let Some(next_item) = self.next_item.take() {
-                let (next_start, next_end) = next_item.clone().into_inner();
+            let (next_start, next_end) = next_range.into_inner();
 
-                // If workspace is empty, just push the next item
-                let Some(best) = self.workspace.first() else {
-                    self.workspace_next_end = min_next_end(&self.workspace_next_end, next_end);
-                    self.workspace.push(next_item);
-                    self.next_item = self.iter.next();
-                    continue; // return to top of the main processing loop
-                };
-                if next_start == *best.start() {
-                    // Always push (this differs from UnionIterMap)
-                    self.workspace_next_end = min_next_end(&self.workspace_next_end, next_end);
-                    self.workspace.push(next_item);
-                    self.next_item = self.iter.next();
-                    continue; // return to top of the main processing loop
-                }
+            assert!(self.end_heap.is_empty()); // cmk
+            self.start_or_min_value = next_start;
+            self.end_heap.push(Reverse(next_end));
+            continue;
 
-                // It does not go into the workspace, so just hold it and keep processing.
-                self.next_item = Some(next_item);
-            }
+            // // If there is a dump_to, dump some.
+            // if let Some(dump_to) = self.dump_to.take() {
+            //     return self.dump_some(dump_to);
+            // }
 
-            // If the workspace is empty, we are done.
-            let Some(best) = self.workspace.first() else {
-                debug_assert!(self.next_item.is_none());
-                debug_assert!(self.ready_to_go.is_none());
-                return self.gather.take();
-            };
+            // // If there is no "next" then return the workspace.
+            // let Some(next_range) = self.iter.next() else {
+            //     return self.dump_all();
+            // };
 
-            // We buffer for output the best item up to the start of the next item (if any).
+            // // Take apart the range and skip if it is empty.
+            // let (next_start, next_end) = next_range.into_inner();
+            // if next_end < next_start {
+            //     continue;
+            // }
 
-            // Find the start of the next item, if any.
-            // unwrap() is safe because we know the workspace is not empty
-            let mut next_end = self.workspace_next_end.take().unwrap();
-            if let Some(next_item) = self.next_item.as_ref() {
-                next_end = min(*next_item.start() - T::one(), next_end);
-            }
+            // // if workspace is empty, set it to the new input and loop
+            // if self.end_heap.is_empty() {
+            //     self.start_or_min_value = next_start;
+            //     self.end_heap.push(Reverse(next_end));
+            //     continue;
+            // }
 
-            // Add the front of best to the gather buffer.
-            if let Some(mut gather) = self.gather.take() {
-                if *gather.end() + T::one() == *best.start() {
-                    if self.workspace.len() % 2 == 1 {
-                        // if the gather is contiguous with the best, then merge them
-                        gather = *gather.start()..=next_end;
-                        self.gather = Some(gather);
-                    } else {
-                        // if an even number of items in the workspace, then flush the gather
-                        self.ready_to_go = Some(gather);
-                        debug_assert!(self.gather.is_none());
-                    }
-                } else {
-                    self.ready_to_go = Some(gather);
-                    // cmk this code appear twice
-                    if self.workspace.len() % 2 == 1 {
-                        self.gather = Some(*best.start()..=next_end);
-                    } else {
-                        debug_assert!(self.gather.is_none());
-                    }
-                }
-            } else {
-                // if there is no gather, then set the gather to the best
-                if self.workspace.len() % 2 == 1 {
-                    self.gather = Some(*best.start()..=next_end);
-                } else {
-                    debug_assert!(self.gather.is_none());
-                }
-            };
+            // debug_assert!(self.start_or_min_value <= next_start); // real assert
+            // if next_start > self.start_or_min_value {
+            //     self.next_range = Some(next_start..=next_end);
+            //     self.dump_to_start(next_start);
+            // }
 
-            // We also update the workspace to removing any items that are completely covered by the new_start.
-            // (Unlike UnionIterMap, we must keep any items that have a lower priority and are shorter than the new best.)
-            // cmk use .filter() ?
-            let mut new_workspace = Vec::new();
-            let mut new_next_end = None;
-            while let Some(item) = self.workspace.pop() {
-                let mut item = item;
-                if *item.end() <= next_end {
-                    // too short, don't keep
-                    // println!("cmk too short, don't keep in workspace {:?}", item.0);
-                    continue; // while loop
-                }
-                item = next_end + T::one()..=*item.end();
-                new_next_end = min_next_end(&new_next_end, *item.end());
-                new_workspace.push(item);
-            }
-            self.workspace = new_workspace;
-            self.workspace_next_end = new_next_end;
-        } // end of main loop
+            // // add the end to the heap
+            // self.end_heap.push(Reverse(next_end));
+        }
     }
 }
 
-// #[allow(dead_code)]
-// fn cmk_debug_string<'a, T, V, VR>(item: &Option<RangeInclusive<T>>) -> String
-// where
-//     T: Integer,
-//     V: ValueOwned,
-//     VR: CloneBorrow<V> + 'a,
-// {
-//     if let Some(item) = item {
-//         format!("Some({:?})", item.0)
-//     } else {
-//         "None".to_string()
-//     }
-// }
+impl<T, I> SymDiffIter<T, I>
+where
+    T: Integer,
+    I: SortedStarts<T>,
+{
+    #[inline]
+    fn count_same_end(&mut self, end: T) -> usize {
+        let mut count = 1usize;
+        while let Some(end2) = self.end_heap.peek() {
+            if end2.0 == end {
+                self.end_heap.pop();
+                count += 1;
+            } else {
+                break;
+            }
+        }
+        count
+    }
+
+    #[inline]
+    fn dump_some(&mut self, dump_to: T) -> RangeInclusive<T> {
+        todo!()
+        // debug_assert!(!self.end_heap.is_empty()); // real assert
+        // debug_assert!(self.start_or_min_value < dump_to); // real assert
+
+        // // Count how many items have the same value as the top item.
+        // let end = self.end_heap.pop().unwrap().0;
+        // let mut count = self.count_same_end(end);
+
+        // if end < dump_to {
+        //     let result = self.start_or_min_value..=end;
+        //     self.start_or_min_value = end + T::one();
+        //     if !self.end_heap.is_empty() {
+        //     self.dump_to = Some(dump_to);
+        //     }
+        //     return result;
+        // } else {
+        //     let result = self.start_or_min_value..=dump_to - T::one();
+        //     self.start_or_min_value = ;
+        // }
+    }
+
+    #[inline]
+    fn dump_all(&mut self) -> Option<RangeInclusive<T>> {
+        loop {
+            // If the workspace is empty, return None.
+            let Some(reverse_end) = self.end_heap.pop() else {
+                return None;
+            };
+
+            // Count how many items have the same value as the top item.
+            let end = reverse_end.0;
+            let mut count = self.count_same_end(end);
+
+            // Find the possible result
+            let result = self.start_or_min_value..=end;
+
+            // move up the start (but avoid overflow)
+            if end < T::safe_max_value() {
+                self.start_or_min_value = end + T::one();
+            } else {
+                debug_assert!(self.end_heap.is_empty()); // real assert
+            }
+
+            // If the count is odd, return the result, otherwise loop.
+            if count % 2 == 1 {
+                return Some(result);
+            }
+        }
+    }
+    // cmk could split this into two functions
+    #[inline]
+    fn dump(&mut self, iter_start: Option<T>) {
+        todo!()
+        // // Count how many items have the same value as the top item.
+        // let mut count = 1usize;
+        // let end = self.end_heap.pop().unwrap().0;
+        // while let Some(end2) = self.end_heap.peek() {
+        //     if end2.0 == end {
+        //         self.end_heap.pop();
+        //         count += 1;
+        //     } else {
+        //         break;
+        //     }
+        // }
+
+        // let end_end = match (iter_start) {
+        //     Some(iter_start) => {
+        //         if end <= iter_start {
+        //             self.keep_it(self.start_or_min_value..=end);
+        //             return;
+        //         }
+        //         end
+        //     }
+        //     None => end,
+        // };
+        // let Some(iter_start) = iter_start else {
+        //     self.keep_it(self.start_or_min_value..=end);
+        //     return;
+        // };
+
+        // if count % 2 == 1 {
+        //     self.keep_it(self.start_or_min_value..=end);
+        // }
+    }
+
+    #[inline]
+    fn keep_it(&mut self, keeper: RangeInclusive<T>) {}
+
+    /// Creates a new [`SymDiffIter`] from zero or more [`SortedDisjointMap`] iterators.
+    /// See [`SymDiffIter`] for more details and examples.
+    pub fn new(mut iter: I) -> Self {
+        Self {
+            iter,
+            start_or_min_value: T::min_value(),
+            end_heap: BinaryHeap::new(),
+            dump_to: None,
+        }
+    }
+}
 
 impl<T, L, R> SymDiffIterMerge<T, L, R>
 where
@@ -217,22 +271,112 @@ where
     }
 }
 
-impl<T, I> SymDiffIter<T, I>
+pub struct SymDiffIter2<T, I>
 where
     T: Integer,
-    I: SortedStarts<T>,
+    I: Iterator<Item = RangeInclusive<T>> + FusedIterator,
+{
+    iter: I,
+    gather: Option<RangeInclusive<T>>,
+}
+
+impl<T, I> FusedIterator for SymDiffIter2<T, I>
+where
+    T: Integer,
+    I: Iterator<Item = RangeInclusive<T>> + FusedIterator,
+{
+}
+
+// cmk0000 review this for simplifications
+impl<T, I> Iterator for SymDiffIter2<T, I>
+where
+    T: Integer,
+    I: Iterator<Item = RangeInclusive<T>> + FusedIterator,
+{
+    type Item = RangeInclusive<T>;
+    // cmk0 does this and UnionIter do the right thing on non-fused input?
+
+    #[inline]
+    fn next(&mut self) -> Option<RangeInclusive<T>> {
+        loop {
+            // If there is no "next" then return gather if it exists.
+            let Some(next) = self.iter.next() else {
+                return self.gather.take();
+            };
+
+            // If there is no gather, set it to next and loop.
+            let Some(gather) = self.gather.take() else {
+                self.gather = Some(next);
+                continue;
+            };
+
+            // Take both next and gather apart.
+            let (next_start, next_end) = next.into_inner();
+            let (gather_start, gather_end) = gather.into_inner();
+
+            // If only used with SymDiffIter2, we can assume gather_end < next_start.
+            debug_assert!(gather_end < next_start); // real assert
+
+            // If they touch, set gather to the union and loop.
+            if gather_end + T::one() == next_start {
+                self.gather = Some(gather_start..=next_end);
+                continue;
+            }
+
+            // Next is disjoint from gather, so return gather and set gather to next.
+            self.gather = Some(next_start..=next_end);
+            return Some(gather_start..=gather_end);
+        }
+    }
+}
+
+impl<T, I> SymDiffIter2<T, I>
+where
+    T: Integer,
+    I: Iterator<Item = RangeInclusive<T>> + FusedIterator,
 {
     /// Creates a new [`SymDiffIter`] from zero or more [`SortedDisjointMap`] iterators.
     /// See [`SymDiffIter`] for more details and examples.
     pub fn new(mut iter: I) -> Self {
-        let item = iter.next();
-        Self {
-            iter,
-            next_item: item,
-            workspace: Vec::new(),
-            workspace_next_end: None,
-            gather: None,
-            ready_to_go: None,
-        }
+        Self { iter, gather: None }
     }
+}
+
+#[test]
+fn sdi2() {
+    let a = [0..=0].into_iter();
+    let iter = SymDiffIter2::new(a);
+    let v = iter.collect::<Vec<_>>();
+    assert_eq!(v, vec![0..=0]);
+
+    let a = [0..=0, 2..=100].into_iter();
+    let iter = SymDiffIter2::new(a);
+    let v = iter.collect::<Vec<_>>();
+    assert_eq!(v, vec![0..=0, 2..=100]);
+
+    let a = [0..=0, 1..=100].into_iter();
+    let iter = SymDiffIter2::new(a);
+    let v = iter.collect::<Vec<_>>();
+    assert_eq!(v, vec![0..=100]);
+
+    // this should debug fail
+    // let mut a = [0..=0, 0..=100].into_iter();
+    // let iter = SymDiffIter2::new(a);
+    // let v = iter.collect::<Vec<_>>();
+    // assert_eq!(v, vec![0..=100]);
+}
+
+#[test]
+fn sdi1() {
+    let a: array::IntoIter<RangeInclusive<i32>, 0> = [].into_iter();
+    let a = AssumeSortedStarts::new(a);
+    let iter = SymDiffIter::new(a);
+    let v = iter.collect::<Vec<_>>();
+    assert_eq!(v, vec![]);
+
+    let a = [0..=0].into_iter();
+    let a = AssumeSortedStarts::new(a);
+    let mut iter = SymDiffIter::new(a);
+    assert_eq!(iter.next(), Some(0..=0));
+    assert_eq!(iter.next(), None);
 }
