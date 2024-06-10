c02d66c8c53c01da62ceba9fc1e368006f6bc6f9
Carl Kadie <carlk@msn.com>
2024-06-10 13:39:44 -0700

    make more wasm tests

--- src/prelude.rs
+++ src/prelude.rs
@@ -10,5 +10,5 @@ pub use crate::{
     DynSortedDisjointMap, IntoString, MultiwayRangeMapBlaze, MultiwayRangeMapBlazeRef,
     MultiwayRangeSetBlaze, MultiwayRangeSetBlazeRef, MultiwaySortedDisjoint,
     MultiwaySortedDisjointMap, RangeMapBlaze, RangeSetBlaze, SortedDisjoint, SortedDisjointMap,
-    SortedStarts, UIntPlusOne,
+    SortedStarts, UIntPlusOne
 };
