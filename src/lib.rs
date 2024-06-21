f9d63e7d7fb598afc59936527a81e9288f3410df
Carl Kadie <carlk@msn.com>
2024-06-21 08:18:55 -0700

    chore: Remove unused code and refactor demo_read_ranges_from_path function

--- src/lib.rs
+++ src/lib.rs
@@ -29,17 +29,12 @@ use std::cmp::Ordering;
 use std::collections::BTreeMap;
 use std::convert::From;
 use std::fmt;
-use std::fs::File;
-use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
 use std::iter::FusedIterator;
 use std::ops::BitOr;
 use std::ops::BitOrAssign;
 use std::ops::Bound;
 use std::ops::RangeBounds;
 use std::ops::RangeInclusive;
-use std::path::Path;
 use std::str::FromStr;
 pub use union_iter::UnionIter;
 pub use unsorted_disjoint::AssumeSortedStarts;
@@ -2128,57 +2123,6 @@ impl<T: Integer, I: SortedDisjoint<T>> SortedDisjoint<T> for NotIter<T, I> {}
 // If the iterator inside Tee is SortedDisjoint, the output will be SortedDisjoint
 impl<T: Integer, I: SortedDisjoint<T>> SortedStarts<T> for Tee<I> {}
 impl<T: Integer, I: SortedDisjoint<T>> SortedDisjoint<T> for Tee<I> {}
-
-#[doc(hidden)]
-pub fn demo_read_ranges_from_path<T, P>(path: P) -> io::Result<RangeSetBlaze<T>>
-where
-    T: FromStr + Integer,
-    P: AsRef<Path>,
-{
-    let lines = BufReader::new(File::open(path)?).lines();
-
-    let mut set = RangeSetBlaze::new();
-    for line in lines {
-        let line = line?;
-        let mut split = line.split_whitespace();
-        let start_str = split.next().ok_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("Missing start of range in line: '{}'", line),
-            )
-        })?;
-        let start = start_str.parse::<T>().map_err(|_| {
-            io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("Invalid start of range '{}' in line: '{}'", start_str, line),
-            )
-        })?;
-
-        let end_str = split.next().ok_or_else(|| {
-            io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("Missing end of range in line: '{}'", line),
-            )
-        })?;
-        let end = end_str.parse::<T>().map_err(|_| {
-            io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("Invalid end of range '{}' in line: '{}'", end_str, line),
-            )
-        })?;
-
-        // Check for extra values
-        if split.next().is_some() {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                format!("Too many values on a line: '{}'", line),
-            ));
-        }
-        set.ranges_insert(start..=end);
-    }
-
-    Ok(set)
-}
 
 #[doc(hidden)]
 pub fn demo_i32_len(range: RangeInclusive<i32>) -> usize {
