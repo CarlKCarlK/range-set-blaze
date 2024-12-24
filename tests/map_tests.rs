8872d670fa6e4cdc2b3dd741febe72d57f9a37d7
Carl Kadie <carlk@msn.com>
2024-12-24 14:38:22 -0800

    make more map tests intregration tests

--- tests/map_tests.rs
+++ tests/map_tests.rs
@@ -5588,3 +5588,401 @@ fn test_empty_inputs_intersection2() {
     let intersection = inputs.intersection();
     assert_eq!(intersection.into_string(), r#"should panic"#);
 }
+
+// use self::map::ValueOwned;
+// use super::*;
+// use crate::intersection_iter_map::IntersectionIterMap;
+// use crate::sorted_disjoint_map::DebugToString;
+// use crate::sorted_disjoint_map::SortedDisjointMap;
+// use crate::sym_diff_iter_map::SymDiffIterMap;
+// use crate::union_iter_map::UnionIterMap;
+// use crate::unsorted_priority_map_map::{AssumePrioritySortedStartsMap, UnsortedPriorityMap};
+// use alloc::collections::BTreeMap;
+// use itertools::Itertools;
+// use rand::seq::SliceRandom;
+// use rand::{rngs::StdRng, Rng, SeedableRng};
+// // cmk what if they forget to import this the thing that lets | work?
+
+// // cmk must test coverage
+
+// #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// fn map_repro_106() {
+//     let input_string = "100e 106b 97c 98c 97e";
+//     let mut input = Vec::<(u8, &u8)>::new();
+//     for pair in input_string.split_whitespace() {
+//         let bytes = pair.as_bytes(); // Get the byte slice of the pair
+//         let c = &bytes[bytes.len() - 1]; // Last byte as char
+//         let num = pair[..pair.len() - 1].parse::<u8>().unwrap();
+//         input.push((num, c)); // Add the (u8, &str) pair to inputs
+//     }
+
+//     let iter = input.clone().into_iter();
+//     let iter = iter.map(|(x, value)| (x..=x, value));
+//     let iter = iter.map(|(range, value)| (range, value));
+//     let iter = UnsortedPriorityMap::new(iter);
+//     let iter = iter.sorted_by(|a, b| {
+//         // We sort only by start -- priority is not used until later.
+//         a.start().cmp(&b.start())
+//     });
+//     let iter = AssumePrioritySortedStartsMap::new(iter);
+//     let iter = UnionIterMap::new(iter);
+//     let vs = format_range_values(iter);
+//     println!("{vs}");
+//     assert_eq!(vs, "97..=98c 100..=100e 106..=106b ");
+
+//     let range_map_blaze = RangeMapBlaze::<u8, u8>::from_iter(input.clone());
+//     assert_eq!(
+//         range_map_blaze.to_string(),
+//         "(97..=98, 99), (100..=100, 101), (106..=106, 98)"
+//     );
+// }
+
+// #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// fn map_repro_206() {
+//     let input_string = "127e 2d 29e 84a 17a 79d 174e 125b 123a 123b 98c 132d 99e 186b 253d 31d 121c 151a 168e 208c 47e 42e 86a 21b 7b 238d 148a 151a 227d 173d 145b 18e 219e 16c 214b 213a 155e 27e 24d 38c 59c 16c 183d 125d 210d 99e 43e 189e 147a 90d 42a 220e 35b 120d 185d 177a 102a 22b 124b 140a 199e 143c 32d 225a 223e 137e 177e 234e 97a 166a 83e 213a 147b 128a 150c 12c 199c 152c 79b 164b 204b 235e 37e 14c 19b 49a 1c 115b 31d 102b 59b 129b 104d 70c 229b 205b 101c 58d 114a 228d 173e 139d 147b 32c 198e 194c 18a 77a 100e 196a 46b 81a 63d 198a 242a 131b 153e 113b 19d 253e 195c 209e 201c 139d 47a 223d 240b 203d 84a 214d 129e 73d 55d 193e 129d 7c 193e 2c 235c 39c 88d 175c 190c 239a 219d 121a 88d 175d 117e 23a 102d 165a 58a 229a 100b 13b 113e 26a 49e 37e 126a 251b 47e 77a 206b ";
+//     let mut input = Vec::<(u8, &u8)>::new();
+//     for pair in input_string.split_whitespace() {
+//         let bytes = pair.as_bytes(); // Get the byte slice of the pair
+//         let c = &bytes[bytes.len() - 1]; // Last byte as char
+//         let num = pair[..pair.len() - 1].parse::<u8>().unwrap();
+//         input.push((num, c)); // Add the (u8, &str) pair to inputs
+//     }
+
+//     let iter = input.clone().into_iter();
+//     let iter = iter.map(|(x, value)| (x..=x, value));
+//     let iter = iter.map(|(range, value)| (range, value));
+//     // let vs = format_range_values(iter);
+//     // println!("{vs}");
+//     // assert_eq!(vs, "127..=127e 2..=2d 29..=29e 84..=84a 17..=17a 79..=79d 174..=174e 125..=125b 123..=123a 123..=123b 98..=98c 132..=132d 99..=99e 186..=186b 253..=253d 31..=31d 121..=121c 151..=151a 168..=168e 208..=208c 47..=47e 42..=42e 86..=86a 21..=21b 7..=7b 238..=238d 148..=148a 151..=151a 227..=227d 173..=173d 145..=145b 18..=18e 219..=219e 16..=16c 214..=214b 213..=213a 155..=155e 27..=27e 24..=24d 38..=38c 59..=59c 16..=16c 183..=183d 125..=125d 210..=210d 99..=99e 43..=43e 189..=189e 147..=147a 90..=90d 42..=42a 220..=220e 35..=35b 120..=120d 185..=185d 177..=177a 102..=102a 22..=22b 124..=124b 140..=140a 199..=199e 143..=143c 32..=32d 225..=225a 223..=223e 137..=137e 177..=177e 234..=234e 97..=97a 166..=166a 83..=83e 213..=213a 147..=147b 128..=128a 150..=150c 12..=12c 199..=199c 152..=152c 79..=79b 164..=164b 204..=204b 235..=235e 37..=37e 14..=14c 19..=19b 49..=49a 1..=1c 115..=115b 31..=31d 102..=102b 59..=59b 129..=129b 104..=104d 70..=70c 229..=229b 205..=205b 101..=101c 58..=58d 114..=114a 228..=228d 173..=173e 139..=139d 147..=147b 32..=32c 198..=198e 194..=194c 18..=18a 77..=77a 100..=100e 196..=196a 46..=46b 81..=81a 63..=63d 198..=198a 242..=242a 131..=131b 153..=153e 113..=113b 19..=19d 253..=253e 195..=195c 209..=209e 201..=201c 139..=139d 47..=47a 223..=223d 240..=240b 203..=203d 84..=84a 214..=214d 129..=129e 73..=73d 55..=55d 193..=193e 129..=129d 7..=7c 193..=193e 2..=2c 235..=235c 39..=39c 88..=88d 175..=175c 190..=190c 239..=239a 219..=219d 121..=121a 88..=88d 175..=175d 117..=117e 23..=23a 102..=102d 165..=165a 58..=58a 229..=229a 100..=100b 13..=13b 113..=113e 26..=26a 49..=49e 37..=37e 126..=126a 251..=251b 47..=47e 77..=77a 206..=206b ");
+
+//     let iter = UnsortedPriorityMap::new(iter);
+//     let iter = iter.sorted_by(|a, b| {
+//         // We sort only by start -- priority is not used until later.
+//         a.start().cmp(&b.start())
+//     });
+//     let iter = AssumePrioritySortedStartsMap::new(iter);
+//     // let vs = format_range_values(iter);
+//     // println!("{vs}");
+//     // assert_eq!(vs, "1..=1c 2..=2c 2..=2d 7..=7c 7..=7b 12..=12c 13..=13b 14..=14c 16..=16c 16..=16c 17..=17a 18..=18a 18..=18e 19..=19d 19..=19b 21..=21b 22..=22b 23..=23a 24..=24d 26..=26a 27..=27e 29..=29e 31..=31d 31..=31d 32..=32c 32..=32d 35..=35b 37..=37e 37..=37e 38..=38c 39..=39c 42..=42a 42..=42e 43..=43e 46..=46b 47..=47e 47..=47a 47..=47e 49..=49e 49..=49a 55..=55d 58..=58a 58..=58d 59..=59b 59..=59c 63..=63d 70..=70c 73..=73d 77..=77a 77..=77a 79..=79b 79..=79d 81..=81a 83..=83e 84..=84a 84..=84a 86..=86a 88..=88d 88..=88d 90..=90d 97..=97a 98..=98c 99..=99e 99..=99e 100..=100b 100..=100e 101..=101c 102..=102d 102..=102b 102..=102a 104..=104d 113..=113e 113..=113b 114..=114a 115..=115b 117..=117e 120..=120d 121..=121a 121..=121c 123..=123b 123..=123a 124..=124b 125..=125d 125..=125b 126..=126a 127..=127e 128..=128a 129..=129d 129..=129e 129..=129b 131..=131b 132..=132d 137..=137e 139..=139d 139..=139d 140..=140a 143..=143c 145..=145b 147..=147b 147..=147b 147..=147a 148..=148a 150..=150c 151..=151a 151..=151a 152..=152c 153..=153e 155..=155e 164..=164b 165..=165a 166..=166a 168..=168e 173..=173e 173..=173d 174..=174e 175..=175d 175..=175c 177..=177e 177..=177a 183..=183d 185..=185d 186..=186b 189..=189e 190..=190c 193..=193e 193..=193e 194..=194c 195..=195c 196..=196a 198..=198a 198..=198e 199..=199c 199..=199e 201..=201c 203..=203d 204..=204b 205..=205b 206..=206b 208..=208c 209..=209e 210..=210d 213..=213a 213..=213a 214..=214d 214..=214b 219..=219d 219..=219e 220..=220e 223..=223d 223..=223e 225..=225a 227..=227d 228..=228d 229..=229a 229..=229b 234..=234e 235..=235c 235..=235e 238..=238d 239..=239a 240..=240b 242..=242a 251..=251b 253..=253e 253..=253d ");
+
+//     let iter = UnionIterMap::new(iter);
+//     let vs = format_range_values(iter);
+//     println!("{vs}");
+//     assert_eq!(vs, "1..=1c 2..=2d 7..=7b 12..=12c 13..=13b 14..=14c 16..=16c 17..=17a 18..=18e 19..=19b 21..=22b 23..=23a 24..=24d 26..=26a 27..=27e 29..=29e 31..=32d 35..=35b 37..=37e 38..=39c 42..=43e 46..=46b 47..=47e 49..=49a 55..=55d 58..=58d 59..=59c 63..=63d 70..=70c 73..=73d 77..=77a 79..=79d 81..=81a 83..=83e 84..=84a 86..=86a 88..=88d 90..=90d 97..=97a 98..=98c 99..=100e 101..=101c 102..=102a 104..=104d 113..=113b 114..=114a 115..=115b 117..=117e 120..=120d 121..=121c 123..=123a 124..=125b 126..=126a 127..=127e 128..=128a 129..=129b 131..=131b 132..=132d 137..=137e 139..=139d 140..=140a 143..=143c 145..=145b 147..=148a 150..=150c 151..=151a 152..=152c 153..=153e 155..=155e 164..=164b 165..=166a 168..=168e 173..=173d 174..=174e 175..=175c 177..=177a 183..=183d 185..=185d 186..=186b 189..=189e 190..=190c 193..=193e 194..=195c 196..=196a 198..=199e 201..=201c 203..=203d 204..=206b 208..=208c 209..=209e 210..=210d 213..=213a 214..=214b 219..=220e 223..=223e 225..=225a 227..=228d 229..=229b 234..=235e 238..=238d 239..=239a 240..=240b 242..=242a 251..=251b 253..=253d ");
+
+//     // let range_map_blaze = RangeMapBlaze::<u8, u8>::from_iter(input.clone());
+//     // assert_eq!(
+//     //     range_map_blaze.to_string(),
+//     //     "(97..=97, 101), (98..=98, 99), (100..=100, 101), (106..=106, 98)"
+//     // );
+// }
+
+// #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// fn map_step_by_step() {
+//     let (s1, s2) = ("a".to_string(), "b".to_string());
+//     let input = [(1, &s2), (2, &s2), (0, &s1)];
+
+//     let iter = input.into_iter();
+//     let iter = iter.map(|(x, value)| (x..=x, value));
+//     let iter = iter.map(|(range, value)| (range, value));
+
+//     let iter = UnsortedPriorityMap::new(iter);
+//     let vs = format!("{:?}", iter.collect::<Vec<_>>());
+//     println!("{vs}");
+//     assert_eq!(
+//         vs,
+//         r#"[Priority { range_value: (1..=2, "b"), priority_number: 0 }, Priority { range_value: (0..=0, "a"), priority_number: 2 }]"#
+//     );
+
+//     let iter = input.into_iter();
+//     let iter = iter.map(|(x, value)| (x..=x, value));
+//     let iter = iter.map(|(range, value)| (range, value));
+//     let iter = UnsortedPriorityMap::new(iter);
+//     let iter = iter..sorted_by(|a, b| {
+//         // We sort only by start -- priority is not used until later.
+//         a.start().cmp(&b.start())
+//     });
+//     let iter = AssumePrioritySortedStartsMap::new(iter);
+//     let vs = format!("{:?}", iter.collect::<Vec<_>>());
+//     println!("{vs}");
+//     assert_eq!(vs, "[Priority { range_value: (0..=0, \"a\"), priority_number: 2 }, Priority { range_value: (1..=2, \"b\"), priority_number: 0 }]");
+
+//     let iter = input.into_iter();
+//     let iter = iter.map(|(x, value)| (x..=x, value));
+//     let iter = iter.map(|(range, value)| (range, value));
+//     let iter = UnsortedPriorityMap::new(iter);
+//     let iter = iter.sorted_by(|a, b| {
+//         // We sort only by start -- priority is not used until later.
+//         a.start().cmp(&b.start())
+//     });
+//     let iter = AssumePrioritySortedStartsMap::new(iter);
+//     let iter = UnionIterMap::new(iter);
+//     let vs = format!("{:?}", iter.collect::<Vec<_>>());
+//     println!("{vs}");
+//     assert_eq!(vs, "[(0..=0, \"a\"), (1..=2, \"b\")]");
+
+//     let range_map_blaze = RangeMapBlaze::<u8, String>::from_iter(input);
+//     println!("{range_map_blaze}");
+//     assert_eq!(range_map_blaze.to_string(), r#"(0..=0, "a"), (1..=2, "b")"#);
+// }
+
+// #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// fn map_repro1() {
+//     let (s1, s2, s3) = ("a".to_string(), "b".to_string(), "c".to_string());
+//     let mut range_map_blaze =
+//         RangeMapBlaze::from_iter([(20..=21, &s1), (24..=24, &s2), (25..=29, &s2)]);
+//     println!("{range_map_blaze}");
+//     assert_eq!(
+//         range_map_blaze.to_string(),
+//         r#"(20..=21, "a"), (24..=29, "b")"#
+//     );
+//     range_map_blaze.internal_add(25..=25, &s3);
+//     println!("{range_map_blaze}");
+//     assert_eq!(
+//         range_map_blaze.to_string(),
+//         r#"(20..=21, "a"), (24..=24, "b"), (25..=25, "c"), (26..=29, "b")"#
+//     );
+// }
+
+// // #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// // #[allow(clippy::bool_assert_comparison)]
+// // fn lib_coverage_0() {
+// //     let a = RangeMapBlaze::from_iter([1..=2, 3..=4]);
+// //     let mut hasher = DefaultHasher::new();
+// //     a.hash(&mut hasher);
+// //     let _d = RangeMapBlaze::<i32>::default();
+// //     assert_eq!(a, a);
+
+// //     let mut set = RangeMapBlaze::new();
+// //     assert_eq!(set.first(), None);
+// //     set.insert(1);
+// //     assert_eq!(set.first(), Some(1));
+// //     set.insert(2);
+// //     assert_eq!(set.first(), Some(1));
+
+// //     let set = RangeMapBlaze::from_iter([(1, "Hello"), (2, "World"), (3, "World")]);
+// //     assert_eq!(set.get(2), Some(2));
+// //     assert_eq!(set.get(4), None);
+
+// //     let mut set = RangeMapBlaze::new();
+// //     assert_eq!(set.last(), None);
+// //     set.insert(1);
+// //     assert_eq!(set.last(), Some(1));
+// //     set.insert(2);
+// //     assert_eq!(set.last(), Some(2));
+
+// //     assert_eq!(a.len(), a.len_slow());
+
+// //     let mut a = RangeMapBlaze::from_iter([1..=3]);
+// //     let mut b = RangeMapBlaze::from_iter([3..=5]);
+
+// //     a.append(&mut b);
+
+// //     assert_eq!(a.len(), 5);
+// //     assert_eq!(b.len(), 0);
+
+// //     assert!(a.contains(1));
+// //     assert!(a.contains(2));
+// //     assert!(a.contains(3));
+// //     assert!(a.contains(4));
+// //     assert!(a.contains(5));
+
+// //     let mut v = RangeMapBlaze::new();
+// //     v.insert(1);
+// //     v.clear();
+// //     assert!(v.is_empty());
+
+// //     let mut v = RangeMapBlaze::new();
+// //     assert!(v.is_empty());
+// //     v.insert(1);
+// //     assert!(!v.is_empty());
+
+// //     let sup = RangeMapBlaze::from_iter([1..=3]);
+// //     let mut set = RangeMapBlaze::new();
+
+// //     assert_eq!(set.is_subset(&sup), true);
+// //     set.insert(2);
+// //     assert_eq!(set.is_subset(&sup), true);
+// //     set.insert(4);
+// //     assert_eq!(set.is_subset(&sup), false);
+
+// //     let sub = RangeMapBlaze::from_iter([1, 2]);
+// //     let mut set = RangeMapBlaze::new();
+
+// //     assert_eq!(set.is_superset(&sub), false);
+
+// //     set.insert(0);
+// //     set.insert(1);
+// //     assert_eq!(set.is_superset(&sub), false);
+
+// //     set.insert(2);
+// //     assert_eq!(set.is_superset(&sub), true);
+
+// //     let a = RangeMapBlaze::from_iter([1..=3]);
+// //     let mut b = RangeMapBlaze::new();
+
+// //     assert_eq!(a.is_disjoint(&b), true);
+// //     b.insert(4);
+// //     assert_eq!(a.is_disjoint(&b), true);
+// //     b.insert(1);
+// //     assert_eq!(a.is_disjoint(&b), false);
+
+// //     let mut set = RangeMapBlaze::new();
+// //     set.insert(3);
+// //     set.insert(5);
+// //     set.insert(8);
+// //     assert_eq!(Some(5), set.0(4..).next());
+// //     assert_eq!(Some(3), set.0(..).next());
+// //     assert_eq!(None, set.0(..=2).next());
+// //     assert_eq!(None, set.0(1..2).next());
+// //     assert_eq!(
+// //         Some(3),
+// //         set.0((Bound::Excluded(2), Bound::Excluded(4))).next()
+// //     );
+
+// //     let mut set = RangeMapBlaze::new();
+
+// //     assert_eq!(set.ranges_insert(2..=5), true);
+// //     assert_eq!(set.ranges_insert(5..=6), true);
+// //     assert_eq!(set.ranges_insert(3..=4), false);
+// //     assert_eq!(set.len(), 5);
+// //     let mut set = RangeMapBlaze::from_iter([(1, "Hello"), (2, "World"), (3, "World")]);
+// //     assert_eq!(set.take(2), Some(2));
+// //     assert_eq!(set.take(2), None);
+
+// //     let mut set = RangeMapBlaze::new();
+// //     assert!(set.replace(5).is_none());
+// //     assert!(set.replace(5).is_some());
+
+// //     let mut a = RangeMapBlaze::from_iter([1..=3]);
+// //     #[allow(clippy::reversed_empty_ranges)]
+// //     a.internal_add(2..=1);
+
+// //     assert_eq!(a.partial_cmp(&a), Some(Ordering::Equal));
+
+// //     let mut a = RangeMapBlaze::from_iter([1..=3]);
+// //     a.extend(std::iter::once(4));
+// //     assert_eq!(a.len(), 4);
+
+// //     let mut a = RangeMapBlaze::from_iter([1..=3]);
+// //     a.extend(4..=5);
+// //     assert_eq!(a.len(), 5);
+
+// //     let mut set = RangeMapBlaze::new();
+
+// //     set.insert(1);
+// //     while let Some(n) = set.pop_first() {
+// //         assert_eq!(n, 1);
+// //     }
+// //     assert!(set.is_empty());
+
+// //     let mut set = RangeMapBlaze::new();
+
+// //     set.insert(1);
+// //     while let Some(n) = set.pop_last() {
+// //         assert_eq!(n, 1);
+// //     }
+// //     assert!(set.is_empty());
+
+// //     let a = RangeMapBlaze::from_iter([1..=3]);
+// //     let i = a.iter();
+// //     let j = i.clone();
+// //     assert_eq!(i.size_hint(), j.size_hint());
+// //     assert_eq!(format!("{:?}", &i), format!("{:?}", &j));
+
+// //     let a = RangeMapBlaze::from_iter([1..=3]);
+// //     let i = a.into_iter();
+// //     assert_eq!(i.size_hint(), j.size_hint());
+// //     assert_eq!(
+// //         format!("{:?}", &i),
+// //         "IntoIter { option_range_front: None, option_range_back: None, into_iter: [(1, 3)] }"
+// //     );
+
+// //     let mut a = RangeMapBlaze::from_iter([1..=3]);
+// //     a.extend([1..=3]);
+// //     assert_eq!(a.len(), 3);
+
+// //     let a = RangeMapBlaze::from_iter([1..=3]);
+// //     let b = <RangeMapBlaze<i32, &str> as Clone>::clone(&a);
+// //     assert_eq!(a, b);
+// //     let c = <RangeMapBlaze<i32, &str> as Default>::default();
+// //     assert_eq!(c, RangeMapBlaze::new());
+
+// //     syntactic_for! { ty in [i8, u8, isize, usize,  i16, u16, i32, u32, i64, u64, isize, usize, i128, u128] {
+// //         $(
+// //             let a = RangeMapBlaze::<$ty>::new();
+// //             println!("{a:#?}");
+// //             assert_eq!(a.first(), None);
+
+// //             let mut a = RangeMapBlaze::from_iter([$ty::one()..=3]);
+// //             let mut b = RangeMapBlaze::from_iter([3..=5]);
+
+// //             a.append(&mut b);
+
+// //             // assert_eq!(a.len(), 5);
+// //             assert_eq!(b.len(), <$ty as Integer>::SafeLen::zero());
+
+// //             assert!(a.contains(1));
+// //             assert!(a.contains(2));
+// //             assert!(a.contains(3));
+// //             assert!(a.contains(4));
+// //             assert!(a.contains(5));
+
+// //             assert!(b.is_empty());
+
+// //             let a = RangeMapBlaze::from_iter([$ty::one()..=3]);
+// //             let b = RangeMapBlaze::from_iter([3..=5]);
+// //             assert!(!a.is_subset(&b));
+// //             assert!(!a.is_superset(&b));
+
+// //         )*
+// //     }};
+
+// //     let a = RangeMapBlaze::from_iter([1u128..=3]);
+// //     assert!(a.contains(1));
+// //     assert!(!a.is_disjoint(&a));
+// // }
+
+// // #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// // #[should_panic]
+// // fn lib_coverage_2() {
+// //     let v = RangeMapBlaze::<u128>::new();
+// //     v.contains(u128::MAX);
+// // }
+
+// // #[test]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+// // #[should_panic]
+// // fn lib_coverage_5() {
+// //     let mut v = RangeMapBlaze::<u128>::new();
+// //     v.internal_add(0..=u128::MAX);
+// // }
+#[test]
+#[allow(clippy::reversed_empty_ranges)]
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
+fn test_range_method_on_range_map_blaze() {
+    let map = RangeMapBlaze::<i32, &str>::from_iter([(1..=3, "a"), (4..=6, "b")]);
+    let expected = RangeMapBlaze::<i32, &str>::from_iter([(3..=3, "a"), (4..=6, "b")]);
+    let a: RangeMapBlaze<i32, &str> = map.range(3..).collect();
+    assert_eq!(a, expected);
+
+    let a: RangeMapBlaze<i32, &str> = map.range(..).collect();
+    assert_eq!(
+        a,
+        RangeMapBlaze::<i32, &str>::from_iter([(1..=3, "a"), (4..=6, "b")])
+    );
+
+    let a: RangeMapBlaze<i32, &str> = map.range(3..7).collect();
+    assert_eq!(a, expected);
+
+    let a: RangeMapBlaze<i32, &str> = map
+        .range((Bound::Excluded(2), Bound::Excluded(7)))
+        .collect();
+    assert_eq!(a, expected);
+}
