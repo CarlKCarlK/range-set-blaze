54b58ccad5dfd4572a9f485a3908ebe414b382ce
Carl Kadie <carlk@msn.com>
2025-05-11 17:15:04 -0700

    update example for ip4

--- examples/routing_ip4.rs
+++ examples/routing_ip4.rs
@@ -1,59 +1,64 @@
 //! cmk000
 
-// use range_set_blaze::prelude::*;
-// use std::net::Ipv4Addr;
+use range_set_blaze::prelude::*;
+use std::net::Ipv4Addr;
+use std::ops::RangeInclusive;
 
-// fn sample1() -> Result<(), Box<dyn std::error::Error>> {
-//     use std::fs::File;
-//     use std::io::BufReader;
+fn sample1() -> Result<(), Box<dyn std::error::Error>> {
+    use std::fs::File;
 
-//     // Until Rust get's 'yield' keyword, to keep things simple, we'll use a Vec
-//     let mut pair_vec: Vec<(RangeInclusive<Ipv4Addr>, (Ipv4Addr, String))> = Vec::new();
+    // Until Rust get's 'yield' keyword, to keep things simple, we'll use a Vec
+    let mut pair_vec: Vec<(RangeInclusive<Ipv4Addr>, (Ipv4Addr, String))> = Vec::new();
+    let mut prev_prefix_len: Option<u32> = None;
+    let mut prev_metric: Option<u32> = None;
+    for line in std::io::BufRead::lines(std::io::BufReader::new(File::open(
+        "./examples/routing_ip4.tsv",
+    )?))
+    .skip(1)
+    {
+        let line = line?;
+        // println!("{}", line);
+        let fields: Vec<&str> = line.split('\t').collect();
+        if fields.len() != 5 {
+            return Err("Expected 5 fields".into());
+        }
 
-//     let mut prev_prefix_len: Option<u32> = None;
-//     let mut prev_metric: Option<u32> = None;
-//     for line in BufRead::lines(io::BufReader::new(File::open(
-//         "./examples/routing_ip4.tsv",
-//     )?))
-//     .skip(1)
-//     {
-//         let line = line?;
-//         // println!("{}", line);
-//         let fields: Vec<&str> = line.split('\t').collect();
-//         assert_eq!(fields.len(), 5, "Expected 5 fields");
+        let destination: Ipv4Addr = fields[0].parse()?;
+        let prefix_len: u32 = fields[1].parse()?;
+        let next_hop: Ipv4Addr = fields[2].parse()?;
+        let interface: &str = fields[3];
+        let metric: u32 = fields[4].parse()?;
 
-//         let destination: Ipv4Addr = fields[0].parse()?;
-//         let prefix_len: u32 = fields[1].parse()?;
-//         let next_hop: Ipv4Addr = fields[2].parse()?;
-//         let interface: &str = fields[3];
-//         let metric: u32 = fields[4].parse()?;
+        if let Some(prev_prefix_len) = prev_prefix_len.replace(prefix_len) {
+            if prev_prefix_len != prefix_len {
+                if prev_prefix_len <= prefix_len {
+                    return Err("Sort by prefix length (desc)".into());
+                }
+                prev_metric = None;
+            }
+        }
 
-//         if let Some(prev_prefix_len) = prev_prefix_len.replace(prefix_len) {
-//             if prev_prefix_len != prefix_len {
-//                 assert!(prev_prefix_len > prefix_len, "Sort by prefix length (desc)");
-//                 prev_metric = None;
-//             }
-//         }
+        if let Some(prev_metric) = prev_metric.replace(metric) {
+            if prev_metric > metric {
+                return Err("Sort by prefix len & metric (asc)".into());
+            }
+        }
 
-//         if let Some(prev_metric) = prev_metric.replace(metric) {
-//             assert!(prev_metric <= metric, "Sort by prefix len & metric (asc)");
-//         }
+        let mask = u32::MAX >> prefix_len;
+        let range_start = Ipv4Addr::from(u32::from(destination) & !mask);
+        let range_end = Ipv4Addr::from(u32::from(destination) | mask);
+        let range = range_start..=range_end;
 
-//         let mask = u32::MAX >> prefix_len;
-//         let range_start = Ipv4Addr::from(u32::from(destination) & !mask);
-//         let range_end = Ipv4Addr::from(u32::from(destination) | mask);
-//         let range = range_start..=range_end;
+        pair_vec.push((range, (next_hop, interface.to_string())));
+    }
 
-//         pair_vec.push((range, (next_hop, interface.to_string())));
-//     }
+    let range_map = RangeMapBlaze::from_iter(pair_vec);
+    for (range, (next_hop, interface)) in range_map.range_values() {
+        println!("{range:?} -> ({next_hop}, {interface})");
+    }
 
-//     let range_map = RangeMapBlaze::from_iter(pair_vec);
-//     for (range, (next_hop, interface)) in range_map.range_values() {
-//         println!("{range:?} -> ({next_hop}, {interface})");
-//     }
-
-//     Ok(())
-// }
+    Ok(())
+}
 
 #[allow(clippy::unwrap_used)]
 fn sample2() {
@@ -95,5 +100,6 @@ fn sample2() {
 }
 
 fn main() {
+    sample1().expect("Failed to run sample1");
     sample2();
 }
